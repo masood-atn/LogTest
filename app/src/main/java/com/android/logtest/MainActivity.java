87b3fd3e55bb2b3fde7894b24ea8453eb30e01a1
Masood Alipour <masood.atn@hotmail.com>
2019-10-11 01:55:56 +0330

    first commit

---
+++ app/src/main/java/com/android/logtest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.android.logtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MYTAG","onCreate State");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MYTAG","onStart State");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYTAG","onResume State");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MYTAG","onPause State");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MYTAG","onStop State");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MYTAG","onDestroy State");
+    }
+}
